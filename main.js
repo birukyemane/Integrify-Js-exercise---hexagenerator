0b135146d9870c768e548b40dd3b9feb5c28371a
birukyemane <bir_yem@yahoo.com>
2018-09-11 14:06:42 +0300

    initial commit - features implemented

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+function randomHexaNumberGenerator(){
+  let randomNum = Math.floor(Math.random()* 10000000);
+  return `#${randomNum.toString(16)}`;
+}
+
+console.log(randomHexaNumberGenerator()); */
+
+
+
